15b9dbf8ff5959c44a2ba19834cfc86b956511e0
Toyboat1000 <68999277+Toyboat1000@users.noreply.github.com>
2022-10-07 16:48:12 -0400

    4

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const btn1 = document.getElementById('button1');
 
 btn1.addEventListener('click', function handleClick() {
     console.log('button1 clicked');
-    window.location.href = 'trivia.html';
+    window.location.href = 'Trivia.html';
 
 });
