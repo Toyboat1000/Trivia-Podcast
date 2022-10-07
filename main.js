bc37964b6e35ddd1bdda6267c38b73157e1dfc5d
Toyboat1000 <68999277+Toyboat1000@users.noreply.github.com>
2022-10-06 21:31:51 -0400

    3

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const btn1 = document.getElementById('button1');
 
 
 btn1.addEventListener('click', function handleClick() {
-  btn1.textContent = "clicked"
-  //redirect page
-  window.location.href = "https://www.google.com";
+    console.log('button1 clicked');
+    window.location.href = "https://www.google.com";
 
 });
