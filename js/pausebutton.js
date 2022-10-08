877909d12839a60a88d7c65fc92d266dbf881211
Toyboat1000 <68999277+Toyboat1000@users.noreply.github.com>
2022-10-07 20:14:56 -0400

    5

--- js/pausebutton.js
+++ js/pausebutton.js
@@ -1,14 +1,18 @@
 const btn2 = document.getElementById('button2');
 var multipurpose2 = "true"
+var audio = new Audio('audio_file.mp3');
+
 
 btn2.addEventListener('click', function handleClick() {
     if (multipurpose2 == "true") {
         btn2.textContent = "Play";
         multipurpose2 = "false";
         console.log('paused');
+        audio.pause();
        
     } 
     if (multipurpose2 == "false") {
+        audio.play();
         btn2.textContent = "Pause";
         console.log('Played');
         multipurpose2 = "true";
