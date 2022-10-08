7e378fe6db13b3e1493805c85551b17918dd9486
Toyboat1000 <68999277+Toyboat1000@users.noreply.github.com>
2022-10-07 20:02:13 -0400

    4

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ btn1.addEventListener('click', function handleClick() {
     window.location.href = 'Trivia.html';
 
 });
+
+const btn2 = document.getElementById('button2');
+var multipurpose2 = "true"
+
+btn2.addEventListener('click', function handleClick() {
+    console.log('Pressed multi purpose button');
+    if (multipurpose2 == "true") {
+        btn2.textContent = "Play";
+    } else {
+        btn2.textContent = "Pause";
+    }
+
+});
