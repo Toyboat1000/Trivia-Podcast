566cd018b875a31cbd6df72d98c82ca9f1413fd4
Toyboat1000 <68999277+Toyboat1000@users.noreply.github.com>
2022-10-07 20:11:42 -0400

    6

--- js/pausebutton.js
+++ js/pausebutton.js
@@ -2,11 +2,11 @@ const btn2 = document.getElementById('button2');
 var multipurpose2 = "true"
 
 btn2.addEventListener('click', function handleClick() {
-    console.log('Pressed multi purpose button');
     if (multipurpose2 == "true") {
         btn2.textContent = "Play";
-        console.log('paused');
         multipurpose2 = "false";
+        console.log('paused');
+       
     } else {
         btn2.textContent = "Pause";
         console.log('Played');
