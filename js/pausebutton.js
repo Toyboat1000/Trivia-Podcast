2d774f52fb3102de278ce12f9306bf5aabb9f67a
Toyboat1000 <68999277+Toyboat1000@users.noreply.github.com>
2022-10-07 20:27:46 -0400

    3

--- js/pausebutton.js
+++ js/pausebutton.js
@@ -1,18 +1,17 @@
 const btn2 = document.getElementById('button2');
 const btn3 = document.getElementById('button3');
 
-var audio = new Audio('audio_file.mp3');
+var audio = new Audio('sounds/Fart123.mp3');
 
 
 btn2.addEventListener('click', function handleClick() {
-        btn2.textContent = "Pause";
         console.log('Played');
         audio.play();
 
 });
 
 btn3.addEventListener('click', function handleClick() {
-    btn3.textContent = "Play";
+
     console.log('paused');
     audio.pause();
 
