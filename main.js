40df1922103ae3778a06da80070a791181bc298f
Toyboat1000 <68999277+Toyboat1000@users.noreply.github.com>
2022-10-07 21:45:57 -0400

    5

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const btn1 = document.getElementById('button1');
 
 btn1.addEventListener('click', function handleClick() {
     console.log('button1 clicked');
-    window.location.href = 'Trivia.html';
+    window.location.href = 'https://htmlpreview.github.io/?https://github.com/Toyboat1000/Trivia-Podcast/blob/main/Trivia.html';
 
 });
