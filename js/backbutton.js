ad4c68ad66a5f82c022c88aaa682868d4c63101c
Toyboat1000 <68999277+Toyboat1000@users.noreply.github.com>
2022-10-11 09:39:14 -0400

    2

---
+++ js/backbutton.js
@@ -0,0 +1,8 @@
+const btn4 = document.getElementById('button4');
+
+
+btn4.addEventListener('click', function handleClick() {
+    console.log('button4 clicked');
+    window.location.href = 'https://htmlpreview.github.io/?https://github.com/Toyboat1000/Trivia-Podcast/blob/main/Index.html';
+
+});
